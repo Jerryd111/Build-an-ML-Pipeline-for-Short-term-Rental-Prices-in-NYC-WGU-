52a8101d64f23da87d2baf30e603721e0395d838
Gerald D'Onofrio <gdonofr@wgu.edu>
2025-01-29 20:00:32 -0500

    Fix errors related to the run.py

--- wandb/nyc_airbnb/src/train_random_forest/run.py
+++ wandb/nyc_airbnb/src/train_random_forest/run.py
@@ -95,10 +95,10 @@ def go(args):
     ######################################
     # Save the sk_pipe pipeline as a mlflow.sklearn model in the directory "random_forest_dir"
     # HINT: use mlflow.sklearn.save_model
-    signature = mlflow.models.infer_signature(X_val, y_pred)
+    #   signature = mlflow.models.infer_signature(X_val, y_pred)
     mlflow.sklearn.save_model(
         # YOUR CODE HERE
-        signature = signature,
+    #       signature = signature,
         input_example = X_train.iloc[:5]
     )
     ######################################
@@ -121,7 +121,7 @@ def go(args):
     # Here we save variable r_squared under the "r2" key
     run.summary['r2'] = r_squared
     # Now save the variable mae under the key "mae".
-    # YOUR CODE HERE
+    run.summary['mae'] = mae
     ######################################
 
     # Upload to W&B the feture importance visualization
